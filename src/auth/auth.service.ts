78ee543275021a61c2febcc5affd35e4532f5211
Phan Gia Dat <phangiadat300106@gmail.com>
2026-08-03 23:06:56 +0700

    Register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const { email, password, fullName } = registerDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('Email này đã được sử dụng');
+    }
+
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        fullName,
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return {
+      message: 'Đăng ký tài khoản thành công',
+      user: userWithoutPassword,
+    };
+  }
+}
